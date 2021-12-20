e8f13dd3968ae44c0412253c930c0a41ec3ec61c
ynaga <ynaga@mail.com>
2021-12-20 15:18:54 +0900

    Initial commit.

---
+++ arqlsanta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  arqlsanta
+//
+//  Created by Yasuhito NAGATOMO on 2021/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ arqlsanta/arqlsantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  arqlsantaApp.swift
+//  arqlsanta
+//
+//  Created by Yasuhito NAGATOMO on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct arqlsantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
